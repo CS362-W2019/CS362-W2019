70bd97b7e1a525151504e921bb94c93410e44177
Michael Patson <patsonm@flip2.engr.oregonstate.edu>
2019-01-24 08:32:27 -0800

    Updated with assignment 2 specs

--- projects/patsonm/dominion/dominion.c
+++ projects/patsonm/dominion/dominion.c
@@ -643,6 +643,117 @@ int getCost(int cardNumber)
   return -1;
 }
 
+// the following func___card are modified for assignment2
+int funcAdventurer(struct gameState *state)
+{
+    
+  int currentPlayer = whoseTurn(state);
+  int nextPlayer = currentPlayer + 1;
+
+  int temphand[MAX_HAND];// moved above the if statement
+  int drawntreasure=0;
+  int cardDrawn;
+  int z = 0;// this is the counter for the temp hand
+    
+    while(drawntreasure<2)
+    {
+	if (state->deckCount[currentPlayer] <1)
+        {//if the deck is empty we need to shuffle discard and add to deck
+          shuffle(currentPlayer, state);
+        }
+        drawCard(currentPlayer, state);
+        cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+        if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+          drawntreasure++;
+        else
+        {
+          temphand[z]=cardDrawn;
+          state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+          z++;
+	    }
+    }
+      while(z-1>=0)
+      {
+	   state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+	   z=z-1;
+      }
+
+    
+    return 0;
+    
+}
+
+int funcSmithy(struct gameState *state, int handPos)
+{
+    int i = 0;
+    int currentPlayer = whoseTurn(state);
+      //+3 Cards
+      for (i = 0; i < 3; i++)
+	{
+	  drawCard(currentPlayer, state);
+	}
+			
+      //discard card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+      return 0;
+}
+
+int funcGreatHall(struct gameState *state, int currentPlayer, int handPos)
+        {
+          //+1 Card
+          drawCard(currentPlayer, state);
+
+          //+1 Actions
+          state->numActions++;
+
+          //discard card from hand
+          discardCard(handPos, currentPlayer, state, 0);
+          return 0;
+        }
+
+ int funcSteward(struct gameState *state, int choice1, int choice2, int choice3, int handPos)
+   {
+          if (choice1 == 1)
+        {
+          //+2 cards
+          drawCard(currentPlayer, state);
+          drawCard(currentPlayer, state);
+        }
+          else if (choice1 == 2)
+        {
+          //+2 coins
+          state->coins = state->coins + 2;
+        }
+          else
+        {
+          //trash 2 cards in hand
+          discardCard(choice2, currentPlayer, state, 1);
+          discardCard(choice3, currentPlayer, state, 1);
+        }
+
+          //discard card from hand
+          discardCard(handPos, currentPlayer, state, 0);
+          return 0;
+    }
+
+  int funcSalvager(struct gameState *state, int choice1, int currentPlayer)
+        {
+          //+1 buy
+          state->numBuys++;
+
+          if (choice1)
+        {
+          //gain coins equal to trashed card
+          state->coins = state->coins + getCost( handCard(choice1, state) );
+          //trash card
+          discardCard(choice1, currentPlayer, state, 1);	
+        }
+
+          //discard card
+          discardCard(handPos, currentPlayer, state, 0);
+          return 0;
+        }
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -666,27 +777,10 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   //uses switch to select card and perform actions
   switch( card ) 
     {
+          //modified per assignmnet 2
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
-			
+        return funcAdventurer(state);
+ 		
     case council_room:
       //+4 Cards
       for (i = 0; i < 4; i++)
@@ -827,17 +921,9 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
 
       return 0;
-		
+	//modified per assignmen2	
     case smithy:
-      //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+        return funcSmithy(state, handPos);
 		
     case village:
       //+1 Card
@@ -900,17 +986,9 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 	    
       
       return 0;
-		
+	//modified per assignmen2	
     case great_hall:
-      //+1 Card
-      drawCard(currentPlayer, state);
-			
-      //+1 Actions
-      state->numActions++;
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+          return funcGreatHall(state, currentPlayer, handPos);
 		
     case minion:
       //+1 action
@@ -962,30 +1040,10 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 				
 	}
       return 0;
-		
+//modified per assignment 2		
     case steward:
-      if (choice1 == 1)
-	{
-	  //+2 cards
-	  drawCard(currentPlayer, state);
-	  drawCard(currentPlayer, state);
-	}
-      else if (choice1 == 2)
-	{
-	  //+2 coins
-	  state->coins = state->coins + 2;
-	}
-      else
-	{
-	  //trash 2 cards in hand
-	  discardCard(choice2, currentPlayer, state, 1);
-	  discardCard(choice3, currentPlayer, state, 1);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
-		
+          return funcSteward(state, choice1, choice2, choice3, handPos);
+          
     case tribute:
       if ((state->discardCount[nextPlayer] + state->deckCount[nextPlayer]) <= 1){
 	if (state->deckCount[nextPlayer] > 0){
@@ -1162,23 +1220,13 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       //discard card
       discardCard(handPos, currentPlayer, state, 0);
       return 0;
-		
+	
+          
+    //per assignmen2	
     case salvager:
-      //+1 buy
-      state->numBuys++;
-			
-      if (choice1)
-	{
-	  //gain coins equal to trashed card
-	  state->coins = state->coins + getCost( handCard(choice1, state) );
-	  //trash card
-	  discardCard(choice1, currentPlayer, state, 1);	
-	}
-			
-      //discard card
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
-		
+    return funcSalvager(state, choice1, currentPlayer); 
+          
+	
     case sea_hag:
       for (i = 0; i < state->numPlayers; i++){
 	if (i != currentPlayer){
