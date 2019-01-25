ef52659f82d6c50e5d4de046c1fd515a0cf7f77d
ziwei wu <ziweiwu@gmail.com>
2019-01-25 01:20:28 -0500

    fixed cutpurse refactor

--- projects/wuzi/dominion/dominion.c
+++ projects/wuzi/dominion/dominion.c
@@ -1020,30 +1020,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3,
       return 0;
 
     case cutpurse:
-      updateCoins(currentPlayer, state, 2);
-      for (i = 0; i < state->numPlayers; i++) {
-        if (i != currentPlayer) {
-          for (j = 0; j < state->handCount[i]; j++) {
-            if (state->hand[i][j] == copper) {
-              discardCard(j, i, state, 0);
-              break;
-            }
-            if (j == state->handCount[i]) {
-              for (k = 0; k < state->handCount[i]; k++) {
-                if (DEBUG)
-                  printf("Player %d reveals card number %d\n", i,
-                         state->hand[i][k]);
-              }
-              break;
-            }
-          }
-        }
-      }
-
-      // discard played card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-
-      return 0;
+      cutpurseAction(currentPlayer, state, handPos);
 
     case embargo:
       //+2 Coins
