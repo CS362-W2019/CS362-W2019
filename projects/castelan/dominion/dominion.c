3e481102a5c233c8c535b61c263429471d276782
Andrew Castellano <castelan@flip1.engr.oregonstate.edu>
2019-01-25 18:34:39 -0800

    refactor for playAdventurer()

--- projects/castelan/dominion/dominion.c
+++ projects/castelan/dominion/dominion.c
@@ -643,6 +643,13 @@ int getCost(int cardNumber)
   return -1;
 }
 
+
+
+
+
+
+
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -667,26 +674,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
-			
+      playAdventurer(state);
     case council_room:
       //+4 Cards
       for (i = 0; i < 4; i++)
@@ -1328,6 +1316,34 @@ int updateCoins(int player, struct gameState *state, int bonus)
   return 0;
 }
 
+int playAdventurer(struct gameState *state){
+  
+        int currentPlayer = whoseTurn(state);
+        int temphand[MAX_HAND];
+        int cardDrawn;
+        int drawntreasure = 0;
+        int z = 0;
+        
+        while(drawntreasure<2){
+	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+    shuffle(currentPlayer, state);
+	}
+	drawCard(currentPlayer, state);
+	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+	  drawntreasure++;
+	else{
+	  temphand[z]=cardDrawn;
+	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+	  z++;
+	}
+      }
+      while(z-1>=0){
+	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+	z=z-1;
+      }
+      return 0;			
+}
 
 //end of dominion.c
 
--- projects/castelan/dominion/dominion_helpers.h
+++ projects/castelan/dominion/dominion_helpers.h
@@ -11,5 +11,7 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
-
+void playSmithy(int card, int choice1, int choice2, int choice3, 
+	       struct gameState *state, int handPos, int *bonus);
+int playAdventurer(struct gameState *state);
 #endif
