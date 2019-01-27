46a1023f2a4d039a8a464c5422a36528605aa80a
byco <30761262+byco@users.noreply.github.com>
2019-01-27 00:23:10 -0800

    Refactored and included new card functions
    Note that the new functions may not have the data necessary to continue game - might need parameters

--- projects/chaobe/dominion/dominion_helpers.h
+++ projects/chaobe/dominion/dominion_helpers.h
@@ -11,5 +11,10 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
+int smithy();
+int adventurer();
+int feast();
+int great_hall();
+int sea_hag();
 
 #endif
