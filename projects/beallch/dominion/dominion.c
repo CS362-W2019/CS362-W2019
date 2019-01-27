1916bbebd7aeecf5f659465c471b1860db9521c5
Christopher Beall <beallch@oregonstate.edu>
2019-01-27 09:51:27 -0600

    last round of bug fixes

--- projects/beallch/dominion/dominion.c
+++ projects/beallch/dominion/dominion.c
@@ -849,7 +849,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
   int drawntreasure=0;
-  int cardDrawn;
   int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
