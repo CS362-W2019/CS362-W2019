f07ac694d7e5100b2c251531711bed19d86b85f7
WeiChien Hsu <weichien711@gmail.com>
2019-02-01 20:04:52 -0600

    :hammer: Refactor testEqual function by passing the reference of test_result

--- projects/hsuweic/dominion/unittest3.c
+++ projects/hsuweic/dominion/unittest3.c
@@ -1,8 +1,6 @@
-/* Testing 
- * 1. 
- * 2. 
- * 3. 
- * 4. 
+/* Testing New Game
+ * 1. Value of gameState pointer should not be NULL after initialized
+ * 2. Size of instance G should be the same as gameState
  */
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -11,9 +9,20 @@
 #include <stdbool.h>
 #include "test_helper.h"
 #include "rngs.h"
+#define SEED 1000
 
 void testNewGame()
 {
+  struct gameState* G = NULL;
+  bool test_result = true;
+  testEqual("There is no instance when we first create the G pointer.", G == NULL, 1, &test_result);
+
+  G = newGame();
+  testEqual("After we call newGame(), G pointer should have value.", G != NULL, 1, &test_result);
+
+  /* Test the size of G pointer */
+  testEqual("Size of the new game should equal to gameState.", sizeof(*G), sizeof(struct gameState), &test_result);
+  testResult(test_result);
 
 }
 
