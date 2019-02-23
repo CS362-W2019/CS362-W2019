ffc18ada960e5ca49e5e6249d135bca3f92b69a7
Tera Schaller <tschaller722@gmail.com>
2019-02-23 12:21:58 -0600

    random testing card2 - sea hag effect, pos control

--- projects/schallet/dominion/randomtestcard2.c
+++ projects/schallet/dominion/randomtestcard2.c
@@ -13,6 +13,24 @@
 #include "rngs.h"
 int main(){
   printf("~~~~~Begining Card 2 Random Testing~~~~~\n");
+  printf("~~~~~Random Testing: sea_hag effect~~~~~\n");
+  printf("Test 1: positive control, call sea_hag effect\n");
+  //need a valid game in progress to call sea_hagEffect
+  //or else it segfaults
+  int myHandPos = 0;
+  int numPlayers = 2;
+  int k[10] = {adventurer, embargo, village, minion, mine,
+    cutpurse,sea_hag, tribute, smithy, council_room};
+  int seed = 1000;
+  int result;
+  //create and initialize a game state
+  struct gameState myState;
+  memset(&myState, 0, sizeof(myState));
+  initializeGame(numPlayers, k, seed, &myState);
+
+  //call villageEffect, should return zero
+  result = sea_hagEffect(&myState);
+  printf("Test result: %d\n", result);
 
 }
 
