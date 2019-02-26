d0b69c653ff5150e79b47e2b97975633b7f1fb3e
Tera Schaller <tschaller722@gmail.com>
2019-02-26 11:49:44 -0600

    randomtestadventurerer

--- projects/schallet/dominion/randomtestadventurer.c
+++ projects/schallet/dominion/randomtestadventurer.c
@@ -18,20 +18,31 @@ int main(){
   printf("Test 1: positive control, call adventurer effect\n");
   //need a valid game in progress to call adventurerEffect
   //or else it segfaults
-  int myHandPos = 0;
+  //int myHandPos = 0;
   int numPlayers = 2;
   int k[10] = {adventurer, embargo, village, minion, mine,
     cutpurse,sea_hag, tribute, smithy, council_room};
   int seed = 1000;
   int result;
+
   //create and initialize a game state
   struct gameState myState;
+  struct gameState myState2;
   memset(&myState, 0, sizeof(myState));
+  memset(&myState2, 0, sizeof(myState));
   initializeGame(numPlayers, k, seed, &myState);
 
-  //call adventurer effect and record result 
-  result = adventurerEffect(&myState);
-  printf("Test result: %d\n", result);
+  //call villiage effect and show result
+  result = villageEffect(&myState, myHandPos);
+  //printf("Test result: %d\n", result);
+  if (result == 0){
+    printf("PASSED\n");
+  } else {
+    printf("FAILED\n");
+  }
+
+
+  printf("~~~~~End random testing of adventurer~~~~~\n");
 
 }
 
