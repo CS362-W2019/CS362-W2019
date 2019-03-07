58ff5db91b4612ffcb8df47c93da461d489a6d8c
laura <lundl@oregonstate.edu>
2019-03-07 12:06:31 -0800

    Setting up combinations of valid urls. Still need to work out some minor issues.

--- projects/lundl/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/lundl/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -19,20 +19,112 @@ public class UrlValidatorTest extends TestCase {
 
    
    
+   /* This function implements manual tests of UrlValidator.isValid() */
    public void testManualTest()
    {
-       //You can use this function to implement your manual testing
-      UrlValidator urlVal = new UrlValidator(null, null, 1);
+      UrlValidator urlVal = new UrlValidator(null,
+              null, 1);
 
+      String[] validSchemes = {"http://", "ftp://", "file://", "https://",
+              "telnet://", ""};
+      String[] invalidSchemes = {"http:/", "://", "http/", "http//", "/"};
+
+      String[] validHosts = {"www.myschool.edu", "myschool.edu",
+              "cs.myschool.edu", "cs.myschool.edu:80", "172.217.11.164"};
+
+      String[] invalidHosts = {
+              "www.thisismyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallylonghostname.org",
+              "mywebsite.garbage", "org", ".com", "myschool.edu:-1",
+              "255.255.255.256"};
+
+      String[] validPaths = {"/", "/cs", "/cs/", "/cs/courses",
+              "cs/courses/list.html", "/cs/courses/list.html#fall%20term", ""};
+
+      String[] invalidPaths = {"cs", "cs/", "/cs/courses/list.html#fall term", "//"};
+
+      String[] validQueries = {"?field=2", "?firstname=Laura&lastname=Lund", ""};
+
+      String[] invalidQueries = {"field=2", "? problem=space", "?marks=2?valid=false"};
+
+
+      /* These tests should all return true */
       boolean expected = true;
-      String url = "http://www.google.com";
-      boolean actual = urlVal.isValid(url);
-      assertEquals(url, expected, actual);
+      String url;
+      // build all possible valid combinations of urls
+       // Iterate through all valid schemes
+       for (String scheme : validSchemes) {
+           StringBuilder urlBuffer = new StringBuilder();
 
-      expected = false;
-      url = "garbage.garbage";
-      actual = urlVal.isValid(url);
-      assertEquals(url, expected, actual);
+           // Start with a valid scheme
+           urlBuffer.append(scheme);
+
+           // Iterate through all valid hosts
+           for (String host : validHosts) {
+               // save start and end indices for host name
+               int hostStart = scheme.length();
+               int hostEnd = host.length() + hostStart;
+
+               // Add this host to the string
+               urlBuffer.append(host);
+
+               // Iterate through all valid paths
+               for (String path : validPaths) {
+                   // Save indices of path
+                   int pathStart = hostEnd;
+                   int pathEnd = pathStart + path.length();
+
+                   // Add path to string
+                   urlBuffer.append(path);
+
+                   // Iterate through all valid queries if current path is
+                   // valid for queries
+                   if ((path != "") && !((path.substring(path.length() - 1) == "/"))) {
+                       for (String query : validQueries) {
+                           // Save indices of query
+                           int queryStart = pathEnd;
+                           int queryEnd = queryStart + query.length();
+
+                           // Add query to string
+                           urlBuffer.append(query);
+
+                           // Convert string builder to string
+                           url = urlBuffer.toString();
+
+                           // test this url
+                           //boolean actual = urlVal.isValid(url);
+                           //assertEquals(url, expected, actual);
+                           System.out.println(url);
+
+                           // remove this query from the string
+                           urlBuffer.delete(queryStart, queryEnd);
+                       }
+                   }
+                   // The previous path is not valid for queries
+                   else {
+                       // Convert string builder to string
+                       url = urlBuffer.toString();
+
+                       // test this url
+                       //boolean actual = urlVal.isValid(url);
+                       //assertEquals(url, expected, actual);
+                       System.out.println(url);
+                   }
+                   // remove this path from the string
+                   urlBuffer.delete(pathStart, pathEnd);
+               }
+               // remove this host from the string
+               urlBuffer.delete(hostStart, hostEnd);
+           }
+       }
+
+      //boolean actual = urlVal.isValid(url);
+      //assertEquals(url, expected, actual);
+
+      /* These tests should all return false */
+      //expected = false;
+      //url = "garbage.garbage";
+      //actual = urlVal.isValid(url);
+      //assertEquals(url, expected, actual);
 
    }
    
