2b1505a520853b8033de344ef57269116e98c6ca
laura <lundl@oregonstate.edu>
2019-03-11 12:36:47 -0700

    Manual Testing Interface complete.

--- projects/lundl/FinalProject/URLValidatorInCorrect/test/ManualTestInterface.java
+++ projects/lundl/FinalProject/URLValidatorInCorrect/test/ManualTestInterface.java
@@ -2,53 +2,66 @@ import java.util.Scanner;
 
 public class ManualTestInterface {
 
-    public static void main(String[] argv) {
+    public static void main(String[] args) {
+        boolean didPass = true;
         System.out.println("Type 'quit' to quit.");
         System.out.println("Please enter a URL for validation:");
-        String[] validUrls = new String[100];
-        int validIndex = 0;
-        String[] invalidUrls = new String[100];
-        int invalidIndex = 0;
 
+        // Get user input from console
         Scanner scanInput = new Scanner(System.in);
-        while (scanInput.hasNext() && (validIndex < 100) && invalidIndex < 100) {
-
-
+        while(scanInput.hasNext()) {
             String userURL = scanInput.next();
+
+
+            // If user chooses to quit
             if(userURL.equals("quit")) {
                 break;
             }
+            else {
+                boolean validAssertion = false;
+                boolean userAssertion = false;
+                while(validAssertion == false) {
+                    System.out.println("Please enter whether this url is valid or invalid.");
+                    System.out.println("Enter v for valid or i for invalid.");
+                    String userAssert = scanInput.next();
+                    if ((userAssert.equals("v")) || (userAssert.endsWith("i"))) {
+                        validAssertion = true;
+                        if(userAssert.equals("v")) {
+                            userAssertion = true;
+                        }
+                    }
+                }
 
-            // UrlValidator object
-            UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+                // UrlValidator object
+                UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 
-            if(urlVal.isValid(userURL) == true) {
-                validUrls[validIndex] = userURL;
-                validIndex++;
-            } else {
-                invalidUrls[invalidIndex] = userURL;
-                invalidIndex++;
+                // Try to validate the user's url
+                try {
+                    boolean result = urlVal.isValid(userURL);
+                    if (result == userAssertion) {
+                        System.out.println("PASSED: " + userURL);
+                    } else {
+                        System.out.println("FAILED: " + userURL);
+                        System.out.println("Expected: " + userAssertion + ", Actual: " + result);
+                        didPass = false;
+                    }
+                }
+                catch(Throwable t) {
+                    System.out.println("FAILED: " + userURL + " with ERROR or EXCEPTION: " + t);
+                    didPass = false;
+                }
             }
 
-            System.out.println(userURL);
-            System.out.println("Please enter a URL for validation:");
         }
-        System.out.println();
-        System.out.println("----- Manual Testing Complete -----");
-        System.out.println("Valid URLs tested:");
-        for(String valid : validUrls) {
-            if (valid != null) {
-                System.out.println(valid);
-            }
-        }
-        System.out.println("\nInvalid URLs tested:");
-        for(String invalid : invalidUrls) {
-            if(invalid != null) {
-                System.out.println(invalid);
-            }
-        }
+
         scanInput.close();
-
-
+        System.out.println("-----Manual Testing Results-----");
+        if(didPass) {
+            System.out.println("    PASS");
+        }
+        else {
+            System.out.println("    FAIL");
+        }
+        System.out.println("-----Manual Testing Complete-----");
     }
 }
